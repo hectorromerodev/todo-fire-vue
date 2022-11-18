b0d66525154fdc5aa0865e9038d0d8f4ab2d7b90
Hector Romero <hectorromerodev@gmail.com>
2022-11-18 15:08:06 -0800

    update navbar ui

--- src/components/NavBar.component.vue
+++ src/components/NavBar.component.vue
@@ -1,66 +1,52 @@
+<script setup lang="ts">
+import { auth } from "@/core/firebase";
+
+/*
+  methods
+*/
+const logout = () => {
+  auth.signOut();
+};
+</script>
 <template>
-    <nav class="navbar" role="navigation" aria-label="main navigation">
-      <div class="navbar-brand">
-        <a class="navbar-item" href="https://todo-e99d3.web.app">
-          <img src="https://firebasestorage.googleapis.com/v0/b/todo-e99d3.appspot.com/o/todo.png?alt=media&token=551b58dd-8205-4c52-aa41-5fd0fc7e0e62" width="32" height="32">
-        </a>
-    
-        <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
-          <span aria-hidden="true"></span>
-          <span aria-hidden="true"></span>
-          <span aria-hidden="true"></span>
-        </a>
-      </div>
-    
-      <div id="navbarBasicExample" class="navbar-menu">
-        <div class="navbar-start">
-
-          <router-link to="/" class="navbar-item">
-            Home
-          </router-link>
-
-    
-          <div class="navbar-item has-dropdown is-hoverable">
-            <a class="navbar-link">
-              More
+  <section class="hero is-fullheight is-default is-bold">
+    <div class="hero-head">
+      <nav class="navbar">
+        <div class="container">
+          <div class="navbar-brand">
+            <a class="navbar-item" href="../">
+              <figure class="is-flex is-align-items-center	">
+                <img
+                  src="https://firebasestorage.googleapis.com/v0/b/todo-e99d3.appspot.com/o/todo-256.png?alt=media&token=8e308f35-e3e7-4309-a3fc-6fb17833f0b0"
+                  alt="Logo">
+                <figcaption class="m-2 title">Todo</figcaption>
+              </figure>
             </a>
-    
-            <div class="navbar-dropdown">
-              <a class="navbar-item">
-                About
-              </a>
-              <a class="navbar-item">
-                Jobs
-              </a>
-              <a class="navbar-item">
-                Contact
-              </a>
-              <hr class="navbar-divider">
-              <a class="navbar-item">
-                Report an issue
-              </a>
+            <span class="navbar-burger burger" data-target="navbarMenu">
+              <span></span>
+              <span></span>
+              <span></span>
+            </span>
+          </div>
+          <div id="navbarMenu" class="navbar-menu">
+            <div class="navbar-end">
+              <div class="tabs is-right">
+                <ul>
+                  <li>
+                    <router-link to="/" class="button is-light mx-1" @click.prevent="logout">Log Out</router-link>
+                  </li>
+                </ul>
+              </div>
             </div>
           </div>
         </div>
+      </nav>
+    </div>
+    <div class="hero-body">
+      <slot default/>
+    </div>
+  </section>
     
-        <div class="navbar-end">
-          <div class="navbar-item">
-            <div class="buttons">
-              
-              <router-link to="register" class="button is-primary">
-                <strong>Sign Up</strong>
-              </router-link>
-
-              <router-link to="sign-in" class="button is-light">
-                Log In
-              </router-link>
-
-            </div>
-          </div>
-        </div>
-      </div>
-    </nav>
-    <slot default/>
 </template>
 
 

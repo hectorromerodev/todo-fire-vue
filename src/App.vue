3c39c96e211ba7e59ff3e8eefa20af7a3e500e11
Hector Romero <hectorromerodev@gmail.com>
2022-11-17 18:18:54 -0800

    removed navBar component from app

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,3 @@
-<script setup lang="ts">
-import NavBarComponent from "./components/NavBar.component.vue";
-
-</script>
 <template>
-  <NavBarComponent>
-    <router-view />
-  </NavBarComponent>
+  <router-view />
 </template>
-
-

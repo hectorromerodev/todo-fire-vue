508fa6e1c429db27c74465f94b315f28e560dd9e
Hector Romero <hectorromerodev@gmail.com>
2022-11-18 15:49:22 -0800

    refactor auth utils

--- src/views/auth/utils/checkEmailPassword.ts
+++ src/views/auth/utils/checkEmailPassword.ts
@@ -1,7 +1,11 @@
 const checkEmailPassword = (email: string, password: string): boolean => {
-  const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
-  return emailRegex.test(email) && passwordRegex.test(password);
+  return checkEmail(email) && passwordRegex.test(password);
 };
 
-export default checkEmailPassword;
+const checkEmail = (email: string): boolean => {
+  const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return emailRegex.test(email);
+};
+
+export {checkEmailPassword, checkEmail};

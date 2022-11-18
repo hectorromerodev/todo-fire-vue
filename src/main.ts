35a943e347af1a7add6b3c48b9aef778ab9edf3c
Hector Romero <hectorromerodev@gmail.com>
2022-11-18 13:47:46 -0800

    add vue-tostification

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,25 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router/index.router";
 
+// Import toast plugin and styles
+import Toast, {POSITION, type PluginOptions} from "vue-toastification";
+import "vue-toastification/dist/index.css";
+
+/*
+  configure toast plugin
+*/
+const toastOptions: PluginOptions = {
+  // You can set your default options here
+  hideProgressBar: true,
+  position: POSITION.TOP_RIGHT,
+  timeout: 4000,
+};
+
+
 import "./assets/main.css";
 
 const app = createApp(App);
 
+app.use(Toast, toastOptions);
 app.use(router);
-
 app.mount("#app");

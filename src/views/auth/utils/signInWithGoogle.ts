1f1b8b0daad31900e34d67a9f2435028ad2cc994
Hector Romero <hectorromerodev@gmail.com>
2022-11-18 16:16:55 -0800

    added sign in with google auth util

---
+++ src/views/auth/utils/signInWithGoogle.ts
@@ -0,0 +1,22 @@
+import { auth } from "@/core/firebase";
+import router from "@/router/index.router";
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { useToast } from "vue-toastification";
+
+const toast = useToast();
+
+const signInGoogle = async () => {
+  const provider = new GoogleAuthProvider();
+  await signInWithPopup(auth, provider)
+  .then((result: any) => {
+    toast.success('Login successful')
+    router.push('/todos');
+  }).catch((error: { code: number, message: string }) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    toast.error(errorMessage)
+    console.error(errorCode, errorMessage);
+  });
+} 
+
+export default signInGoogle;

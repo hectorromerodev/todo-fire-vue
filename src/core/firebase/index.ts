1df68fc0e4cd4356b115732f15b24c47ec49f6f7
Hector Romero <hectorromerodev@gmail.com>
2022-11-18 22:43:19 -0800

    lint file

--- src/core/firebase/index.ts
+++ src/core/firebase/index.ts
@@ -1,7 +1,7 @@
 import { initializeApp, type FirebaseOptions } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
-import { getAnalytics } from "firebase/analytics";
+import { getAnalytics } from 'firebase/analytics';
 
 const firebaseConfig: FirebaseOptions = {
   apiKey: import.meta.env.VITE_API_KEY,
@@ -17,6 +17,5 @@ const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const auth = getAuth(app);
 const analytics = getAnalytics(app);
-
 
 export { db, auth, analytics };

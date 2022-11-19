73d4e439dc9a7cc6c4b5ec5abd2a258eb5b4e0a5
Hector Romero <hectorromerodev@gmail.com>
2022-11-18 19:08:56 -0800

    add firebase analytics

--- src/core/firebase/index.ts
+++ src/core/firebase/index.ts
@@ -16,7 +16,7 @@ const app = initializeApp(firebaseConfig);
 
 const db = getFirestore(app);
 const auth = getAuth(app);
-getAnalytics(app);
+const analytics = getAnalytics(app);
 
 
-export { db, auth };
+export { db, auth, analytics };
